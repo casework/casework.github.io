7de11e30b41d9ddde8914c3a99734e7122a80b91
Ryan <ryan@lab214.com>
2020-01-21 14:59:41 -0500

    added world map to members page

---
+++ assets/js/map-data.js
@@ -0,0 +1,18 @@
+var membershipData = {
+  "US": 26, // United States
+  "KR": 7, // South Korea
+  "GB": 6, // United Kingdom
+  "SE": 4, // Sweden
+  "NL": 3, // Netherlands
+  "CA": 2, // Canada
+  "CH": 3, // Switzerland
+  "AU": 1, // Australia
+  "BE": 1, // Belgium
+  "DE": 1, // Germany
+  "FR": 2, // France
+  "HR": 1, // Croatia
+  "IE": 1, // Ireland
+  "IL": 1, // Israel
+  "IT": 1, // Italy
+  "TW": 1, // Taiwan
+  };
--- assets/js/members.js
+++ assets/js/members.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
+    $('#world-map').vectorMap({
+      map: 'world_mill',
+      focusOn: {
+        region: 'US',
+        x: 0.1,
+        y: 0.1,
+        scale: 2.5,
+        animate: true
+      },
+      series: {
+        regions: [{
+          values: membershipData,
+          scale: ['#C8EEFF', '#0071A4'],
+          normalizeFunction: 'polynomial'
+        }]
+      },
+      onRegionTipShow: function(e, el, code){
+        if (membershipData[code]) {
+          el.html(el.html()+' ('+membershipData[code]+')');
+        } else {
+          el.html(el.html()+' (0)');
+        }
+      }
+    });
 
     $(".filter-button").click(function(){
         var value = $(this).attr('data-filter');
