92690903dc1e15f1aa3f787a2551fda3a769cb2e
Alex Nelson <alexander.nelson@nist.gov>
2020-08-18 16:06:10 -0400

    Build JSON-LD of "Asgard" example into separate file
    This patch takes the JSON-LD that was hard-coded in the HTML of the
    "Asgard" example page, and breaks it out into separate JSON files that
    compile into both the HTML page and a self-contained JSON-LD file.
    
    The Makefile recursion from the top directory was updated to descend
    into examples directory, and then into each example with a Makefile.
    
    This accomplishes a part of ONT-129, assisting with reviewing for the
    CASE 0.2.0 release (ONT-352).
    
    This patch includes a confirmation that the updated
    `make clean && make && make check` path work as written for the
    examples/ directory.  I unfortunately found that I'd forgotten the
    middle `make` in the just-merged commit for ONT-353, though, and will be
    updating that separately.
    
    References:
    * [ONT-129] JSON-LD examples on the website may need to be
      machine-readable
    * [ONT-352] Test website examples against release-0.2.0 branch
    * [ONT-356] Prepare "Asgard" JSON-LD for querying
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

---
+++ examples/asgard/asgard_json.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+# This software was developed at the National Institute of Standards
+# and Technology by employees of the Federal Government in the course
+# of their official duties. Pursuant to title 17 Section 105 of the
+# United States Code this software is not subject to copyright
+# protection and is in the public domain. NIST assumes no
+# responsibility whatsoever for its use by other parties, and makes
+# no guarantees, expressed or implied, about its quality,
+# reliability, or any other characteristic.
+#
+# We would appreciate acknowledgement if the software is used.
+
+"""
+This single-purpose script combines the portions of the Asgard JSON-LD
+into a single JSON-LD file.  This script is likely to be replaced with
+an alternative mechanism.
+"""
+
+__version__ = "0.1.0"
+
+import json
+import sys
+
+def main():
+    obj = None
+    sys.stderr.write("args.in_json = %r." % args.in_json)
+    for in_json in args.in_json:
+        with open(in_json, "r") as in_fh:
+            in_obj = json.load(in_fh)
+            if obj is None:
+                obj = in_obj
+            else:
+                for item in in_obj:
+                    obj["uco-core:object"].append(item)
+
+    json.dump(obj, sys.stdout, indent=4)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("in_json", nargs="+")
+    args = parser.parse_args()
+    main()
