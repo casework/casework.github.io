f1af6db067e58350fa770e602ab7295c6a97bd7a
Alex Nelson <alexander.nelson@nist.gov>
2021-10-04 16:29:33 -0400

    Start unified semi-static render workflow with ONT-434 Makefiles
    These were copied as-is from their current state in the CASE-Examples
    ONT-434 branch, and will be revised and integrated in follow-on patches.
    
    References:
    * [AC-209] Use single "Semi-static" rendering strategy for website
    * [ONT-434] (CP-33) CASE needs to provide an inference object
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

---
+++ examples/src/example-src.mk
@@ -0,0 +1,106 @@
+#!/usr/bin/make -f
+
+# This software was developed at the National Institute of Standards
+# and Technology by employees of the Federal Government in the course
+# of their official duties. Pursuant to title 17 Section 105 of the
+# United States Code this software is not subject to copyright
+# protection and is in the public domain. NIST assumes no
+# responsibility whatsoever for its use by other parties, and makes
+# no guarantees, expressed or implied, about its quality,
+# reliability, or any other characteristic.
+#
+# We would appreciate acknowledgement if the software is used.
+
+SHELL := /bin/bash
+
+top_srcdir := $(shell cd ../../../.. ; pwd)
+
+drafting_pkl_sources := $(wildcard ontology/*.ttl)
+
+illustration_name := inference
+illustration_snippets_json := $(wildcard $(illustration_name)-*.json)
+query_sparql_files := $(wildcard query-*.sparql)
+query_md_files := $(foreach query_sparql_file,$(query_sparql_files),$(subst .sparql,.md,$(query_sparql_file)))
+
+generated_readme_sed_sources := \
+  $(illustration_snippets_json) \
+  $(query_md_files) \
+  $(query_sparql_files)
+
+all: \
+  generated-README.md \
+  generated-$(illustration_name).json
+
+.PHONY: \
+  normalize
+
+check: \
+  check-0.4.0
+
+check-0.4.0: \
+  drafting.pkl \
+  generated-$(illustration_name).json
+	source $(top_srcdir)/venv/bin/activate \
+	  && validate \
+	    drafting.pkl \
+	    generated-$(illustration_name).json
+
+clean:
+	@rm -f \
+	  *.sed \
+	  .normalized-* \
+	  drafting.pkl \
+	  generated-* \
+	  query-*.md
+
+drafting.pkl: \
+  $(drafting_pkl_sources) \
+  $(top_srcdir)/.dependencies.done.log
+	source $(top_srcdir)/venv/bin/activate \
+	  && serialize \
+	    -c drafting \
+	    -o _$@ \
+	    ontology
+	mv _$@ $@
+
+generated-README.md: \
+  README.md.in \
+  generated-README.sed
+	sed -f generated-README.sed README.md.in > _$@
+	mv _$@ $@
+
+generated-README.sed: \
+  $(illustration_name)_base.json \
+  $(generated_readme_sed_sources)
+	for x in $^ ; do \
+          echo "/@$$(echo $${x} | tr '[:lower:]' '[:upper:]' | tr . _ | tr - _)@/r $${x}" ; \
+          echo "/@$$(echo $${x} | tr '[:lower:]' '[:upper:]' | tr . _ | tr - _)@/d" ; \
+	done >> _$@
+	mv _$@ $@
+
+generated-$(illustration_name).json: \
+  $(illustration_name)_json.py \
+  $(illustration_name)_base.json \
+  $(illustration_snippets_json)
+	python3 $(illustration_name)_json.py \
+	  $(illustration_name)_base.json \
+	  $(illustration_snippets_json) \
+	  > _$@
+	mv _$@ $@
+
+normalize:
+	ls $(illustration_name)-*.json \
+	  | while read x; do python3 -m json.tool $$x .normalized-$$x && mv .normalized-$$x $$x ; done
+
+query-%.md: \
+  query-%.sparql \
+  $(top_srcdir)/.venv.done.log \
+  ../drafting.ttl \
+  generated-$(illustration_name).json
+	source $(top_srcdir)/venv/bin/activate \
+	  && case_sparql_select \
+	    _$@ \
+	    $< \
+	    ../drafting.ttl \
+	    generated-$(illustration_name).json
+	mv _$@ $@
